dcacdeb0c8148fa05d5632a86fa1fea4a2d49c08
craigneilson-fayreplay <craig.neilson@thefayreplay.co.uk>
2025-10-26 10:30:19 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,30 +8,4 @@ root.render(
     <ProtectedGlasgowForecast />
   </React.StrictMode>
 );
-```
-4. Click **"Commit new file"**
 
-**Add file 5: `src/App.js`**
-1. Click **"Add file"** > **"Create new file"**
-2. Name it: `src/App.js`
-3. Paste your entire **App.js** code from the project files
-4. Click **"Commit new file"**
-
-**Add file 6: `.gitignore`**
-1. Click **"Add file"** > **"Create new file"**
-2. Name it: `.gitignore`
-3. Paste:
-```
-/node_modules
-/.pnp
-.pnp.js
-/coverage
-/build
-.DS_Store
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
